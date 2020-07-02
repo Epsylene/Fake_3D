adcb4de18c53beb779b8ac0ab150e11c20a92fad
Victor Rodriguez <victorodriguez148@gmail.com>
2020-07-02 21:44:26 +0200

    "Still" simulation

---
+++ 1_Still/src/pSObject.js
@@ -0,0 +1,15 @@
+class pSObject {
+    constructor(x, y, z, l) {
+        this.pos = {x, y, z};
+        this.l = l;
+    }
+
+    update(dt, everyObjects) { }
+
+    draw(drawer)
+    {
+        drawer.noStroke()
+              .fill(255, 255, 255)
+              .rect(this.pos.x, this.pos.y, this.l, this.l);
+    }
+}
