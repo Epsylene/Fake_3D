5c9303b03fb5ffa1c957d72ebc29595ae0bed0d7
Victor Rodriguez <victorodriguez148@gmail.com>
2020-07-04 18:06:19 +0200

    Added commentaries to "Portals" simulation

--- 3_Portals/src/pSObject.js
+++ 3_Portals/src/pSObject.js
@@ -11,6 +11,8 @@ class pSObject {
     }
 
     addPortal() {
+        //On prend différentes valeurs aléatoires pour
+        // x, y, v et l...
         let x = this.randomInt(-50, 50);
         let y = this.randomInt(-50, 50);
         let z = 1;
@@ -18,15 +20,23 @@ class pSObject {
         let l = this.randomInt(10, 30);
         let p = 1;
 
+        //... et on ajoute à notre tableau un élément
+        // avec ces valeurs.
         this.portals.push({x, y, z, v, l, p});
     }
 
     update(dt, everyObjects) {
+        //Pour chaque élément dans le tableau...
         for (var i = 0; i < this.portals.length; i++) {
+            //...on a le même code que pour un carré seul.
             this.portals[i].z += this.portals[i].v * dt;
             this.portals[i].p = this.fl / this.portals[i].z;
         }
 
+        //Si t est divisible par 100 (autrement dit: si t est
+        // un multiple de 100), alors on ajoute un portail.
+        //La valeur est arbitraire, on ne cherche qu'à éviter
+        // d'ajouter trop de portails, trop vite.
         if(this.t % 100 == 0) {
             this.addPortal();
         }
@@ -36,6 +46,7 @@ class pSObject {
 
     draw(drawer)
     {
+        //Même commentaire que pour update().
         for (var i = 0; i < this.portals.length; i++) {
             drawer.noStroke()
                   .fill(255, 255, 255)
@@ -44,6 +55,5 @@ class pSObject {
                         this.portals[i].l * this.portals[i].p,
                         this.portals[i].l * this.portals[i].p);
         }
-
     }
 }
